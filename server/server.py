735619ee07283eefad2ad1ad705072ef72ad3020
Perry Yang <byang33@ucsc.edu>
2020-04-09 17:58:32 -0700

    pages

--- server/server.py
+++ server/server.py
@@ -2,6 +2,13 @@ import json
 import pymongo
 from pymongo import (MongoClient,)
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+
+world_cases = 0
+usa_cases = 0
+
+
 
 client = pymongo.MongoClient("mongodb+srv://Perry998:test@cluster0-bd2oz.mongodb.net/test?retryWrites=true&w=majority")
 db = client.get_database("covid")
@@ -13,8 +20,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])    # testing
 def index():
-    data = col.find_one({"state": "Alabama", "county": "Alabama"})
-    return render_template('index.html', data = data)
+    world, usa = front()
+    return {'world': world, 'usa': usa}
 
 @app.route('/<state>/<county>', methods=['GET'])    # get confirmed and death cases of covid-19 for each county
 def getData(state, county):
@@ -27,7 +34,30 @@ def getData(state, county):
         data["confirmed"] = int(c["confirmed"])
     return render_template('index.html', data = data)
 
+def world():
+    page = requests.get('https://www.worldometers.info/coronavirus/')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    class_list = soup.find(class_ = 'maincounter-number')
+    span_list= class_list.find('span')    
+    for x in span_list:
+        world_cases = x
+        break
+    return world_cases
 
+def USA():
+    page = requests.get('https://www.worldometers.info/coronavirus/')
+    soup = BeautifulSoup(page.text, 'html.parser')
+    table = soup.find('tbody')
+    td_list = table.find('td', attrs={'style': 'font-weight: bold; text-align:right'})
+    for x in td_list:
+        usa_cases = x
+        break
+    return usa_cases
+
+def front():
+    world_cases = world()
+    usa_cases = USA()
+    return world_cases, usa_cases
 
 if __name__ == '__main__':
     app.run()
